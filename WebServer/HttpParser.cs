f82d56451f7a6bd92e7ac1d04673658b6ed9e9b3
TeeMensch <jonathanasmussen@gmail.com>
2025-09-30 15:04:35 +0200

    build base for http parser

--- WebServer/HttpParser.cs
+++ WebServer/HttpParser.cs
@@ -4,7 +4,26 @@ namespace Parser
 {
     class HttpParser
     {
-        static async Task<string> GetVersion(string request)
+        public static async Task<string> GetMethod(string request)
+        {
+            
+        }
+        public static async Task<string> GetVersion(string request)
+        {
+            
+        }
+
+        public static async Task<string> GetDomain(string request)
+        {
+            
+        }
+
+        public static async Task<string> GetHeader(string request)
+        {
+            
+        }
+
+        public static async Task<string> GetBody(string request)
         {
             
         }
